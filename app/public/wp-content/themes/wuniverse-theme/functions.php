e076652bcfcfeb6274b6643efdb9057f435616dd
Alison Wu <alison.wu23@gmail.com>
2023-10-12 09:03:01 +1000

    feat: Interior Page Template, title-tag, site_url()

--- app/public/wp-content/themes/wuniverse-theme/functions.php
+++ app/public/wp-content/themes/wuniverse-theme/functions.php
@@ -2,6 +2,7 @@
 
 <?php
 
+// loading js and css files
 function wuniverse_files() {
   wp_enqueue_script('main-wus-blog-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
@@ -11,3 +12,10 @@ function wuniverse_files() {
 }
 
 add_action('wp_enqueue_scripts', 'wuniverse_files');
+
+// setup title-tag on browser
+function wuniverse_features() {
+  add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'wuniverse_features');
