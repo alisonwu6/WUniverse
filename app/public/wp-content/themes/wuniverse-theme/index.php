f598106b5cce1a21fc645da1631a747a13bd37cb
Alison Wu <alison.wu23@gmail.com>
2023-10-11 13:42:28 +1000

    practice: php function & blogInfo()

--- app/public/wp-content/themes/wuniverse-theme/index.php
+++ app/public/wp-content/themes/wuniverse-theme/index.php
@@ -1,1 +1,20 @@
-This is my WUniverse Theme!
+<?php 
+  // echo 2+2
+
+  // this is function definition
+  function myFirstFunction() {
+    echo 2 + 2;
+  }
+  // call
+  // myFirstFunction();
+
+  function greet($name, $color) {
+    echo "<p>Hi, my name is $name and my favorite color is $color. </p>";
+  }
+
+  greet("Alison", "yellow");
+  greet("John", "seagreen");
+?>
+
+<h1><?php blogInfo("name")?></h1>
+<p><?php blogInfo("description")?></p>
