6518e080e98fb3c3a2f60c7310c4fb67fc3a28bc
Alison Wu <alison.wu23@gmail.com>
2023-10-14 15:46:45 +1000

    feat: custom post types - event & single-event & archive-event

---
+++ app/public/wp-content/mu-plugins/wuniverse_post_types.php
@@ -0,0 +1,20 @@
+<?php 
+
+function wuniverse_post_types() {
+  register_post_type('event', array(
+    'rewrite' => array('slug' => 'events'),   // changing slug into plural
+    'has_archive' => true,
+    'public' => true,
+    'show_in_rest' => true,                   // using the modern block editor
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => "Edit Event",
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
+}
+
+add_action('init', 'wuniverse_post_types');
