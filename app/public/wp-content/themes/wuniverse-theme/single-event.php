181f0ba5fcb1d8d26e7688257a384cf765a0a79e
Alison Wu <alison.wu23@gmail.com>
2023-10-16 14:06:36 +1000

    feat: Displaying Relationships between single-event and single-program

--- app/public/wp-content/themes/wuniverse-theme/single-event.php
+++ app/public/wp-content/themes/wuniverse-theme/single-event.php
@@ -22,11 +22,25 @@
             <?php the_title(); ?>
           </span>
         </p>
-      </div>    
+      </div>
       
       <div class="generic-content">
         <?php the_content(); ?>
       </div>
+
+      <?php 
+        $relatedPrograms = get_field('related_programs');    
+
+        if ($relatedPrograms) {
+          echo '<hr class="section-break">';
+          echo '<h4 class="headline headline--medium">Related Program(s)</h4>';
+          echo '<ul class="link-list min-list">';
+          foreach($relatedPrograms as $program) { ?>
+            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+          <?php }
+          echo '</ul>';
+        }
+      ?>
     </div>
   <?php }
   get_footer();
