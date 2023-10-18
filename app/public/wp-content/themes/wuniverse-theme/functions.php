f338b20a975c7107cdfe8731a4b6d9c2544d6695
Alison Wu <alison.wu23@gmail.com>
2023-10-18 21:33:37 +1000

    feat: loading googleMap api & template of campus

--- app/public/wp-content/themes/wuniverse-theme/functions.php
+++ app/public/wp-content/themes/wuniverse-theme/functions.php
@@ -32,6 +32,7 @@ function pageBanner($args = NULL) {
 
 // loading js and css files
 function wuniverse_files() {
+  wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyC6Blnd187x-o5R_IemhFhtcD8cPw-u-ao', NULL, '1.0', true);
   wp_enqueue_script('main-wus-blog-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"');
