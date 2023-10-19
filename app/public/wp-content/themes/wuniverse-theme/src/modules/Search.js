2b20ec83a3eb31da89ae9438277901d730053c72
Alison Wu <alison.wu23@gmail.com>
2023-10-19 12:50:50 +1000

    feat: setting keyboard events for opening and closing method

--- app/public/wp-content/themes/wuniverse-theme/src/modules/Search.js
+++ app/public/wp-content/themes/wuniverse-theme/src/modules/Search.js
@@ -7,22 +7,36 @@ class Search {
     this.closeButton = $('.search-overlay__close');
     this.searchOverlay = $('.search-overlay');
     this.events();
+    this.isOverlayOpen = false;
   }
   // 2. events
   events() {
     // jquery on click function 
     this.openButton.on('click', this.openOverlay.bind(this)); 
     this.closeButton.on('click', this.closeOverlay.bind(this));
+    $(document).on('keydown', this.keyPressDispatcher.bind(this));
   }
 
   // 3. methods (functions, action...)
   // open up and reveal itself
+  keyPressDispatcher(e) {
+    if (e.keyCode == 83 && !this.isOverlayOpen) {
+      this.openOverlay();
+    }
+    if (e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
   openOverlay() {
     this.searchOverlay.addClass("search-overlay--active");
+    $("body").addClass("body-no-scroll");
+    this.isOverlayOpen = true;
   }
   // close down or hide itself
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active");
+    $("body").removeClass("body-no-scroll");
+    this.isOverlayOpen = false;
   }
 }
 
