639d03d08e2dac227b99fb1c3b09a488fe6269cd
Alison Wu <alison.wu23@gmail.com>
2023-10-21 07:32:13 +1000

    feat: Conditional Logic Within Template Literal

--- app/public/wp-content/themes/wuniverse-theme/functions.php
+++ app/public/wp-content/themes/wuniverse-theme/functions.php
@@ -33,11 +33,15 @@ function pageBanner($args = NULL) {
 // loading js and css files
 function wuniverse_files() {
   wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyC6Blnd187x-o5R_IemhFhtcD8cPw-u-ao', NULL, '1.0', true);
-  wp_enqueue_script('main-wus-blog-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+  wp_enqueue_script('main-wuniverse-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"');
   wp_enqueue_style('wuniverse_main_styles', get_theme_file_uri('/build/style-index.css'));
   wp_enqueue_style('wuniverse_extra_styles', get_theme_file_uri('/build/index.css'));
+
+  wp_localize_script('main-wuniverse-js', 'wuniverseData', array(
+    'root_url' => get_site_url(),
+  ));
 }
 
 add_action('wp_enqueue_scripts', 'wuniverse_files');
