0c1d6ac9fe5c9a328995f79d84074605691f22b6
Alison Wu <alison.wu23@gmail.com>
2023-10-26 14:25:23 +1000

    feat: search logic - aware of relationships

--- app/public/wp-content/themes/wuniverse-theme/includes/search-route.php
+++ app/public/wp-content/themes/wuniverse-theme/includes/search-route.php
@@ -77,5 +77,31 @@ function wuniverseSearchResults($data) {
     }
   }
 
+  $programRelationShipQuery = new WP_Query(array(
+    'post_type' => 'professor',
+    'meta_query' => array(
+      array(
+        'key' => 'related_programs',
+        'compare' => 'LIKE',
+        'value' => '"87"'
+      )
+    )
+  ));
+
+  while($programRelationShipQuery->have_posts()) {
+    $programRelationShipQuery->the_post();
+
+    if (get_post_type() == 'professor') {
+      array_push($results['professors'], array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink(),
+        'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+      ));
+    }
+  }
+
+  // $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR)); // it seems no need to use array_values
+  $results['professors'] = array_unique($results['professors'], SORT_REGULAR);
+
   return $results;
 }
