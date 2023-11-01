262cebaa038c0e4102d153aea70b46f1ba00daf1
Alison Wu <alison.wu23@gmail.com>
2023-11-01 10:46:06 +1000

    feat:create my notes page & custom post type

--- app/public/wp-content/mu-plugins/wuniverse_post_types.php
+++ app/public/wp-content/mu-plugins/wuniverse_post_types.php
@@ -70,6 +70,22 @@ function wuniverse_post_types() {
     ),
     'menu_icon' => 'dashicons-welcome-learn-more'
   ));
+
+  // Note Post Type
+  register_post_type('note', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'public' => false,
+    'show_ui' => true, // show in admin
+    'labels' => array(
+      'name' => 'Notes',
+      'add_new_item' => 'Add New Note',
+      'edit_item' => "Edit Note",
+      'all_items' => 'All Notes',
+      'singular_name' => 'Note'
+    ),
+    'menu_icon' => 'dashicons-welcome-write-blog'
+  ));
 }
 
 add_action('init', 'wuniverse_post_types');
