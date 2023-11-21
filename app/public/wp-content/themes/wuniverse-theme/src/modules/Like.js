e5b49675cf0061b4f511642645d0c2d8b9cdcb68
Alison Wu <alison.wu23@gmail.com>
2023-11-21 13:58:38 +1000

    feat: create and delete like post

--- app/public/wp-content/themes/wuniverse-theme/src/modules/Like.js
+++ app/public/wp-content/themes/wuniverse-theme/src/modules/Like.js
@@ -13,7 +13,8 @@ class Like {
   ourClickDispatcher(e) {
     var currentLikeBox = $(e.target).closest(".like-box");
 
-    if (currentLikeBox.data("exists") == "yes") {
+    // if you want to pull in fresh updated attr values, always use the attr method.
+    if (currentLikeBox.attr("data-exists") == "yes") {
       this.deleteLike(currentLikeBox);
     } else {
       this.createLike(currentLikeBox);
@@ -31,6 +32,11 @@ class Like {
         'professorId': currentLikeBox.data('professor')
       },
       success: (response) => {
+        currentLikeBox.attr('data-exists', 'yes'); 
+        var likeCount = parseInt(currentLikeBox.find('.like-count').html(), 10);
+        likeCount++;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", response);
         console.log(response)
       },
       error: (response) => {
@@ -39,14 +45,20 @@ class Like {
     })
   }
 
-  deleteLike() {
+  deleteLike(currentLikeBox) {
     $.ajax({
       beforeSend: (xhr) => {
         xhr.setRequestHeader('X-WP-Nonce', wuniverseData.nonce);
       },
       url: wuniverseData.root_url + '/wp-json/wuniverse/v1/manageLike',
+      data: {'like': currentLikeBox.attr("data-like")},
       type: 'DELETE',
       success: (response) => {
+        currentLikeBox.attr('data-exists', 'no'); 
+        var likeCount = parseInt(currentLikeBox.find('.like-count').html(), 10);
+        likeCount--;
+        currentLikeBox.find(".like-count").html(likeCount);
+        currentLikeBox.attr("data-like", "");
         console.log(response)
       },
       error: (response) => {
