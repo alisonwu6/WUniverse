7776f3507e6e81603ef4ee960138e1e6326e4e77
Alison Wu <alison.wu23@gmail.com>
2023-11-25 14:33:41 +1000

    chore: moved the .git file inward to app/public

--- app/public/wp-content/themes/wuniverse-theme/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'd29387ca79c90f0d7007');
---
+++ wp-content/themes/wuniverse-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '5e3249ed36c491a8bcea');
