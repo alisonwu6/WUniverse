22213c706dcdcd7ffdf0080aeedc2a41d3514324
Alison Wu <alison.wu23@gmail.com>
2023-12-13 19:38:17 +1000

    feat: Finishing Word Filter Plugin

--- wp-content/plugins/our-word-filter-plugin/index.php
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -14,6 +14,25 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 class OurWordFilterPlugin {
   function __construct() {
     add_action('admin_menu', array($this, 'ourMenu'));
+    add_action('admin_init', array($this, 'ourSettings'));
+    if (get_option('plugin_words_to_filter')) add_filter('the_content', array($this, 'filterLogic'));
+  }
+
+  function ourSettings() {
+    add_settings_section('replacement-text-section', null, null, 'word-filter-options');
+    register_setting('replacementFields', 'replacementText');
+    add_settings_field('relacement-text', 'Filtered Text', array($this, 'replacementFieldHTML'), 'word-filter-options', 'replacement-text-section');
+  }
+
+  function replacementFieldHTML() { ?>
+    <input type="text" name="replacementText" value="<?php echo esc_attr(get_option('replacementText', '***')) ?>">
+    <p class="description">Leave blank to simply remove the filtered words</p>
+  <?php }
+
+  function filterLogic($content) {
+    $badWords = explode(',', get_option('plugin_words_to_filter'));
+    $badWordsTrimmed = array_map('trim', $badWords);
+    return str_replace($badWordsTrimmed, esc_html(get_option('replacementText', '****')), $content);
   }
 
   function ourMenu() {
@@ -58,7 +77,17 @@ class OurWordFilterPlugin {
   <?php }
 
   function optionsSubPage() {?>
-    Hello world from option page.
+    <div class="wrap">
+      <h1>Word FIlter Options</h1>
+      <form action="options.php" method="POST">
+        <?php
+          settings_errors();
+          settings_fields('replacementFields');
+          do_settings_sections('word-filter-options');
+          submit_button();
+        ?>
+      </form>
+    </div>
   <?php }
 }
 
