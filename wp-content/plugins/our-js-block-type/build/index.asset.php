ba8e5b244f4fe809feabade83063b398acb5c81e
Alison Wu <alison.wu23@gmail.com>
2023-12-14 17:21:29 +1000

    feat: Setting React Component - EditComponent

--- wp-content/plugins/our-js-block-type/build/index.asset.php
+++ wp-content/plugins/our-js-block-type/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react'), 'version' => 'fd53f0ace8a15d991b24');
+<?php return array('dependencies' => array('react', 'wp-components'), 'version' => '019247bcee76e1abd7a7');
--- wp-content/plugins/our-js-block-type/src/index.js
+++ wp-content/plugins/our-js-block-type/src/index.js
@@ -1,27 +1,61 @@
+import "./index.scss"
+import { TextControl, Flex, FlexBlock, FlexItem, Button, Icon } from "@wordpress/components";
+
 wp.blocks.registerBlockType("ourplugin/our-js-block-type", {
   title: "Our Js Block Type",
   icon: "smiley",
   category: "common",
   attributes: {
-    skyColor: {type: "string"},
-    grassColor: {type: "string"},
+    question: { type: "string" },
+    answers: { type: "array", default: ["red", "blue", "green"]},
   },
-  edit: function (props) {
-    function updateSkyColor(event) {
-      props.setAttributes({skyColor: event.target.value});
-    }
-    function updateGrassColor(event) {
-      props.setAttributes({grassColor: event.target.value});
-    }
-
-    return (
-      <div>
-        <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
-        <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
-      </div>
-    )
-  },
+  edit: EditComponent,
   save: function (props) {
     return null
   }
 })
+
+function EditComponent(props) {
+  function updateQuestion(value) {
+    props.setAttributes({question: value})
+  }
+
+  function deleteAnswer(indexToDelete) {
+    console.log('props.attributes.answers', props.attributes.answers)
+    const newAnswers = props.attributes.answers.filter(function(x, index) {
+      return index != indexToDelete;
+    })
+    props.setAttributes({answers: newAnswers})
+  }
+
+  return (
+    <div className="our-js-block-type">
+      <TextControl label="Question:" value={props.attributes.question} onChange={updateQuestion} style={{fontSize: "20px"}} />
+      <p style={{ fontSize: "13px", margin: "20px 0px 8px 0px" }}>Answers:</p>
+      {props.attributes.answers.map(function (answer, index) {
+        return (
+          <Flex>
+            <FlexBlock>
+              <TextControl value={answer} onChange={newValue => {
+                const newAnswers = props.attributes.answers.concat([]);
+                newAnswers[index] = newValue;
+                props.setAttributes({answers: newAnswers});
+              }} />
+            </FlexBlock>
+            <FlexItem>
+              <Button>
+                <Icon className="mark-as-correct" icon="star-empty" />
+              </Button>
+            </FlexItem>
+            <FlexItem>
+              <Button isLink className="block-delete" onClick={() => deleteAnswer(index)}>Delete</Button>
+            </FlexItem>
+          </Flex>
+        )
+      })}
+      <Button isPrimary onClick={() => {
+        props.setAttributes({answers: props.attributes.answers.concat([""])})
+      }}>Add another answer</Button>
+    </div>
+  )
+}
