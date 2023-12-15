a9be2304c8717ffcccb9afaececd0f22ffb09231
Alison Wu <alison.wu23@gmail.com>
2023-12-15 13:18:17 +1000

    feat: Passing Block Data From PHP Into JavaScript / React

--- wp-content/plugins/our-js-block-type/build/frontend.asset.php
+++ wp-content/plugins/our-js-block-type/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'abd3e915fc88db7bfd24');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '5af759782aa8a06fe668');
